3c376236bd069b9489e397d3488a9517c9a40624
razia <razia321@gmail.com>
2020-04-22 15:33:49 +0600

    difference between equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,11 @@
+const first = 2;
+const second = 3;
+if (first === second) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+// (==)don't check data-type-- that sign convert type with other and try to make them same
+// (===)check value and data-type both
+// true compare with 1
+// false compare with 0
