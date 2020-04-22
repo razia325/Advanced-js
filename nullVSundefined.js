be39def4fc49f0d1d2c8b6d863101eab18da0d1c
razia <razia321@gmail.com>
2020-04-22 15:16:37 +0600

    null vs undefined clear

---
+++ nullVSundefined.js
@@ -0,0 +1,22 @@
+// let bird;
+// console.log(bird);
+
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return;
+// }
+// const result = add(12, 12);
+// console.log(result);
+
+// function add(num1, num2) {
+//     console.log(num1, num2);
+// }
+// const result = add(13);
+// console.log(result);
+
+// const result = { name: "x", roll: "y" };
+// console.log(result.sname);
+let fun = undefined;
+console.log(fun);
+
+// null
